12d5907ae2747cf70d3e42c88310059c2c7991ce
David Mariscal Fernández <david.mariscal.fernandez.09@gmail.com>
2026-09-14 00:58:46 +0200

    Add package init

---
+++ src/curbshade/__init__.py
@@ -0,0 +1,5 @@
+from .model import RoutingProfile, EdgeEvaluation
+from .router import route_graph, RouteResult, load_graph
+
+__all__ = ["RoutingProfile", "EdgeEvaluation", "route_graph", "RouteResult", "load_graph"]
+__version__ = "0.1.0"
